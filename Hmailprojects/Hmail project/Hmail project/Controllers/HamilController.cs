0da261490ddde8dfc7ad043ff724543ba3ecd73e
Vishal <vishalgptdeos95@gamil.com>
2026-03-17 11:22:01 +0530

    new hmail porject created

---
+++ Hmailprojects/Hmail project/Hmail project/Controllers/HamilController.cs
@@ -0,0 +1,20 @@
+﻿using Hmail_project.Helper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using Newtonsoft.Json;
+
+namespace Hmail_project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HamilController : ControllerBase
+    {
+        [HttpGet(Name = "GetHmailInbox")]
+        public IActionResult GetHmailInbox()
+        {
+            List<MimeMessage> data =   (new ServiceHelper()).GetInbox();
+            return Ok(JsonConvert.SerializeObject(data));
+        }
+    }
+}
---
+++ Hmailprojects/Hmail project/Hmail project/Helper/ServiceHelper.cs
@@ -0,0 +1,33 @@
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MimeKit;
+
+namespace Hmail_project.Helper
+{
+    public class ServiceHelper
+    {
+        public List<MimeMessage> GetInbox()
+        {
+            using var client = new ImapClient();
+
+            client.Connect("localhost", 143, false);
+
+            client.Authenticate("info@vishalEnterprises.com", "Vk9795235876@#");
+
+            var inbox = client.Inbox;
+
+            inbox.Open(FolderAccess.ReadOnly);
+
+            var messages = new List<MimeMessage>();
+
+            for (int i = 0; i < inbox.Count; i++)
+            {
+                messages.Add(inbox.GetMessage(i));
+            }
+
+            client.Disconnect(true);
+
+            return messages;
+        }
+    }
+}
